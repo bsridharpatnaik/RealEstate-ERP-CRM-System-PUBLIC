7f4c15ea0abc8d8fbfaddc3f7b3d33ab632cd7ff
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2021-06-06 23:21:49 +0530

    SMTP Issue fix

--- sc-inventory-service/src/main/java/com/ec/application/config/EmailConstants.java
+++ sc-inventory-service/src/main/java/com/ec/application/config/EmailConstants.java
@@ -2,7 +2,7 @@ package com.ec.application.config;
 
 public final class EmailConstants 
 {
-	public static final String mailHost = "evergreencity.in";
+	public static final String mailHost = "mail.evergreencity.in";
 	public static final String mailPort="465";
 	public static final String mailUsername="stockupdate@evergreencity.in";
 	public static final String mailPassword = "REDACTED";
