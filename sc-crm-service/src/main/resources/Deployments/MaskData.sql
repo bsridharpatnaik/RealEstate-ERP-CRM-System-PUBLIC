fc4f1ce2ae9f02bd6ad0e2c8d0121008672c5448
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2024-05-10 22:53:58 +0530

    Mask Data SQL

---
+++ sc-crm-service/src/main/resources/Deployments/MaskData.sql
@@ -0,0 +1,6 @@
+use egcity;
+set SQL_SAFE_UPDATES=0;
+-- UPDATE customer_lead SET email_id=REPEAT('*', CHAR_LENGTH(email_id) - 4) ;
+-- UPDATE customer_lead SET primary_mobile=CONCAT(SUBSTR(primary_mobile, 1,4), REPEAT('*', 6));
+-- UPDATE customer_lead SET secondary_mobile=CONCAT(SUBSTR(secondary_mobile, 1,4), REPEAT('*', CHAR_LENGTH(secondary_mobile) - 4));
+-- UPDATE security_user SET password='$2a$04$3svnMZ.hWVGet1sx5GkcreszK3OCuSR6Ybqx/E7yB50gbM7J4DVQq';
