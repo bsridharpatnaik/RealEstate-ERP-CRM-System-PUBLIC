ee3c089442518fa737d1f4ed36ec16d3ad0eab85
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2024-05-13 22:16:53 +0530

    Update CreateViews.sql

--- sc-crm-service/src/main/resources/CreateViews.sql
+++ sc-crm-service/src/main/resources/CreateViews.sql
@@ -211,6 +211,25 @@ FROM   (SELECT CASE WHEN cl.propertytype IS NULL THEN 'OTHERS' ELSE  cl.property
                   GROUP  BY cl.propertyType) AS tx
               ON tx.propertyType = y.propertyType;
 
+-- Payments Page
+CREATE OR REPLACE VIEW payments_page AS
+SELECT
+	cl.lead_id,
+    cl.name,
+    CASE WHEN cps.payment_date IS NULL THEN '' ELSE DATE_FORMAT(cps.payment_date, "%Y-%m-%d") END as paymentDate,
+    cps.amount,
+    CASE WHEN cps.isReceived=true THEN 'Received' ELSE 'Pending' END as isReceived,
+    CASE WHEN cps.isCustomerPayment=true THEN 'Customer' ELSE 'Bank' END as paymentFrom,
+    su.user_name,
+    pt.propertyType,
+    pn.name as propertyName
+FROM customer_deal_structure cds
+INNER JOIN customer_lead cl ON cds.lead_id=cl.lead_id
+INNER JOIN security_user su on su.user_id=cl.user_id
+LEFT JOIN customer_payment_schedule cps ON cps.deal_id=cds.deal_id AND cps.is_deleted=0
+LEFT JOIN property_type pt ON pt.property_type_id=cds.property_type_id
+LEFT JOIN property_name pn ON pn.property_name_id=cds.property_name_id
+WHERE cds.is_Deleted=0 ;
 -- Activities for dashboard
 set @q=concat('CREATE OR REPLACE VIEW activities_for_dashboard AS
 SELECT UUID() as id,\'today\' as type, su.user_name,COUNT(leadactivity_id) as count FROM LeadActivity la
