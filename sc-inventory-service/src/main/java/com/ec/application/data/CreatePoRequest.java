ae690b77887cac961a44fe368ea771f97e4df94c
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2026-01-12 22:06:30 +0530

    sa

--- sc-inventory-service/src/main/java/com/ec/application/data/CreatePoRequest.java
+++ sc-inventory-service/src/main/java/com/ec/application/data/CreatePoRequest.java
@@ -4,12 +4,18 @@ import java.util.*;
 import com.ec.application.Deserializers.ToSentenceCaseDeserializer;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreatePoRequest {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
     @NonNull
     private Date poDate;
     private Long supplierId;
--- sc-inventory-service/src/main/java/com/ec/application/data/IndentInventoryData.java
+++ sc-inventory-service/src/main/java/com/ec/application/data/IndentInventoryData.java
@@ -1,16 +1,23 @@
 package com.ec.application.data;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.DateDeserializers;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.lang.NonNull;
 
 import java.util.Date;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class IndentInventoryData {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    @JsonDeserialize(using = DateDeserializers.DateDeserializer.class)
     @NonNull
     Date indentDate;
 
