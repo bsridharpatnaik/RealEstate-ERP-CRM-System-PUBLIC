bf8232c4c445073204e49095f419c1b591824794
Sridhar Patnaik <bsridharpatnaik@gmail.com>
2026-03-11 21:50:27 +0530

    Update IndentInwardEntry.java

--- sc-inventory-service/src/main/java/com/ec/application/model/IndentInwardEntry.java
+++ sc-inventory-service/src/main/java/com/ec/application/model/IndentInwardEntry.java
@@ -1,9 +1,6 @@
 package com.ec.application.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -14,6 +11,7 @@ import java.util.Date;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(of = "inwardId")
 public class IndentInwardEntry {
 
     @Column(name = "inward_id", nullable = false, length = 50)
